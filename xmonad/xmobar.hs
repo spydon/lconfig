c26093334f636a2719efc4698b83a2e5d9c8612b
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2020-06-09 20:36:54 +0200

    Too many requests to stack overflow

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -23,15 +23,15 @@ Config {
         "-h", "green",
         "-l", "red"
         ] 600,
+        Run Com "/bin/bash" ["-c", "~/scripts/so-flame.sh"] "flame" 2000,
         Run Com "/bin/bash" ["-c", "~/scripts/altcoin.sh BTC"] "bitcoin" 600,
         Run Com "/bin/bash" ["-c", "~/scripts/altcoin.sh ETH"] "ethereum" 600,
         Run Com "/bin/bash" ["-c", "~/scripts/altcoin.sh XMR"] "monero" 600,
         Run Com "/bin/bash" ["-c", "~/scripts/altcoin.sh XRP"] "ripple" 600,
         Run Com "/bin/bash" ["-c", "~/scripts/altcoin.sh EOS"] "eos" 600,
-        Run Com "/bin/bash" ["-c", "python3 ~/scripts/stock.py DIGN.ST"] "dignitana" 600,
         Run StdinReader],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader%  }{ %dignitana% | %bitcoin% | %ethereum% | %eos% | %monero% | %ripple% | %multicpu% | %dynnetwork% | %memory% | %battery% | <fc=#ee9a00>%date% </fc>"
+    template = "%StdinReader%  }{ %flame% | %bitcoin% | %ethereum% | %eos% | %monero% | %ripple% | %multicpu% | %dynnetwork% | %memory% | %battery% | <fc=#ee9a00>%date% </fc>"
 }
 
